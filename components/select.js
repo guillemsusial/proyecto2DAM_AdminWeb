54a83f2ec0df5847af9e766f62563aaf4fb92a22
1tsKalisey <ramendez2002@gmail.com>
2023-05-17 20:20:58 +0200

    DB updated

--- components/select.js
+++ components/select.js
@@ -1,30 +1,24 @@
-import axios from 'axios';
-import React, { useMemo, useState, useEffect } from 'react';
+import axios from 'axios'
+import React, { useMemo, useState, useEffect } from 'react'
 
-import Tables from './table';
+import Tables from './table'
+import { Button } from '@chakra-ui/button'
 
 function Seleccionar() {
-  const [data, setData] = useState([]);
-  console.log(data);
+  const [data, setData] = useState([])
+  console.log(data)
   useEffect(() => {
-      const datas = {
-        dataSource: 'Proyecto2DAM',
-        database: 'CarWikiAR',
-        collection: 'cars'
-      };
-      (async () => {
-        const result =  await axios.post(
-          '/api/data',
-          datas
-        )
-       
-      setData(result.data.documents);
-        
-      
-      
-      })();
-  }, []);
+    const datas = {
+      dataSource: 'Proyecto2DAM',
+      database: 'CarWikiAR',
+      collection: 'cars'
+    }
+    ;(async () => {
+      const result = await axios.post('/api/data', datas)
 
+      setData(result.data.documents)
+    })()
+  }, [])
 
   // useEffect(() => {
   //   (async () => {
@@ -32,6 +26,12 @@ function Seleccionar() {
   //     setData(result.data);
   //   })();
   // }, []);
+  const Edit = () => {
+    return <Button bg={'orange'} />
+  }
+  const Delete = () => {
+    return <Button bg={'red'} />
+  }
 
   const columns = useMemo(
     () => [
@@ -39,24 +39,50 @@ function Seleccionar() {
         Header: 'Documents',
         columns: [
           {
-            Header: 'Name',
-            accessor: 'nombre'
+            Header: 'ID',
+            accessor: '_id'
           },
           {
-            Header: 'Type',
-            accessor: '_id'
+            Header: 'Nombre',
+            accessor: 'nombre'
+          }
+        ]
+      },
+      {
+        Header: 'General Info',
+        columns: [
+          {
+            Header: 'Fabricante',
+            accessor: 'datos_identificativos.fabricante'
+          },
+          {
+            Header: 'Modelo',
+            accessor: 'datos_identificativos.modelo'
+          }
+        ]
+      },
+      {
+        Header: 'Options',
+        columns: [
+          {
+            Header: 'Update',
+            accessor: Edit
+          },
+          {
+            Header: 'Delete',
+            accessor: Delete
           }
         ]
       }
     ],
     []
-  );
+  )
 
   return (
     <div className="App">
       <Tables columns={columns} data={data} />
     </div>
-  );
+  )
 }
 
-export default Seleccionar;
+export default Seleccionar
