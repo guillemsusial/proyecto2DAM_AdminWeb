35c81954ff3f26a755840c56ca06656deb17f699
guillemsusial <guillemsusial@gmail.com>
2023-05-28 23:05:50 +0200

    3d model names OK

--- pages/ModelViewer.js
+++ pages/ModelViewer.js
@@ -1,4 +1,4 @@
-import { Box, Link } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 
@@ -10,13 +10,6 @@ export default function ModelViewer() {
   
   return (
     <Box w={'100%'}>
-      <Link
-        id="hola"
-        href="intent://arvr.google.com/scene-viewer/1.0?mode=ar_preferred&disable_occlusion=true&file=http%3A%2F%2F172.17.40.175%3A3000%2F3DModels%2Ftoyota_gt_apex_trueno_ae86_stock_jdm.glb#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=http%3A%2F%2F172.17.40.175%3A3000%2FModelViewer%23model-viewer-no-ar-fallback;end;"
-        isExternal
-      >
-        asdasdasd
-      </Link>
       <model-viewer
         style={{ height: '500px', width: '100%' }}
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
